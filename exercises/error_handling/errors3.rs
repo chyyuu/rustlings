28aabd102007c2c7d358679fe399c85226fb5468
Yu Chen <yuchen@mail.tsinghua.edu.cn>
2020-05-23 23:41:37 +0800

    one day work

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,7 +4,7 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
@@ -12,7 +12,11 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let the_cost = total_cost(pretend_user_input);
+    let cost = match the_cost {
+        Ok(v) => v,
+        Err(e) => 0,
+    };
 
     if cost > tokens {
         println!("You can't afford that many!");
--- exercises/error_handling/errorsn.rs
+++ exercises/error_handling/errorsn.rs
@@ -15,21 +15,26 @@
 // type goes where the question marks are, and how do we return
 // that type from the body of read_and_validate?
 //
-// Execute `rustlings hint errorsn` for hints :)
+// Execute `rustlings hint errorsn` for hintrustlings hint errorsn`s :)
 
-// I AM NOT DONE
+
 
 use std::error;
 use std::fmt;
 use std::io;
+// enum myError {
+//     io::Error,
+//     ParseIntError,
+//     CreationError
+// };
 
 // PositiveNonzeroInteger is a struct defined below the tests.
-fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, ???> {
+fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, Box<dyn error::Error>> {
     let mut line = String::new();
-    b.read_line(&mut line);
-    let num: i64 = line.trim().parse();
-    let answer = PositiveNonzeroInteger::new(num);
-    answer
+    b.read_line(&mut line)?;
+    let num: i64 = line.trim().parse()?;
+    let answer = PositiveNonzeroInteger::new(num)?;
+    Ok(answer)
 }
 
 //
